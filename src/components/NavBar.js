ae7947b3d1bb8f0f319ec6025d86f856c94071fa
+ <+>
2023-11-18 02:11:47 +0300

    modified NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Home from "./Home";
+import About from "./About";
+import ProjectItem from "./ProjectItem";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>
+    <a href="#home">Home </a>
+    <a href="#about">About </a>
+    <a href="#home">Projects </a>
+    
+    
+    {/* display an <a> tag for each link here */} 
+  </nav>;
 }
 
 export default NavBar;
